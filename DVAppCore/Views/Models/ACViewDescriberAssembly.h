05623bf898ea70e06fc2d3c82b5567514edf2457
Denis Vashkovski <denis.vashkovski.vv@gmail.com>
2018-11-20 09:57:42 +0300

    added ACViewDescriberAssembly

---
+++ DVAppCore/Views/Models/ACViewDescriberAssembly.h
@@ -0,0 +1,11 @@
+//
+//  ACViewDescriberAssembly.h
+//  DVAppCore
+//
+//  Created by Denis Vashkovski on 20/11/2018.
+//  Copyright © 2018 Denis Vashkovski. All rights reserved.
+//
+
+@protocol ACViewDescriberAssembly <NSObject>
++ (Class)viewClass;
+@end
